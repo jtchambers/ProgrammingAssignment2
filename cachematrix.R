e8e735649a44617e8f0ab9f2c7c729422937471a
jtchambers <jtchambers@crimson.ua.edu>
2015-09-23 22:24:11 -0500

    ProgrammingAssignment2 Updated and Completed
    Here are my two functions, makeCacheMatrix and cacheSolve, that returns a matrix that is the inverse of 'x'.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions will take a special matrix and
+## cache the inverse of the matrix.
+## The purpose of these functions is to compute an inverse(or any value) of a matrix
+##(in this example) more efficiently by caching value if it is not changing as it
+## goes through the loop.
 
-## Write a short comment describing this function
+## makeCacheMatrix - Function that creates a special matrix, that is actually a list
+## Function will set and get value of the matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inverse <- NULL
+  set <- function(y) {
+    ## <<- assigns a value from the current environment 
+    ##to an object from a different environment
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inv) inverse <<- inv
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - Function that calculates the inverse of the special matrix above
+## If it already has been calculated, it gets it from the cache, then it shows the matrix. 
+## Else it calculates the matrix, stores it via the cache, then shows it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  ## Returning a matrix that is the inverse of 'x'
+  
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("Getting Cached Data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- inv(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
+
+## Sources: I used the templates given in the example on the Programming 
+## Assignment 2 Instructions page.
